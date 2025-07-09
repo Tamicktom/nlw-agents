120718f7a3425bc2ea3b577f193e4d1c1ea35a42
Tamicktom <rickpokemon38@gmail.com>
2025-07-09 02:28:33 -0300

    feat: implement CreateRoomPage component and loading/error states

--- web/src/routes/create-room.tsx
+++ web/src/routes/create-room.tsx
@@ -1,10 +1,39 @@
 //* Libraries imports
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
+
+//* Hooks imports
+import { useGetRooms } from "@/hooks/rooms/use-get-rooms";
 
 export const Route = createFileRoute('/create-room')({
-  component: RouteComponent,
+  component: CreateRoomPage,
 })
 
-function RouteComponent() {
-  return <div>Hello "/create-room"!</div>
+function CreateRoomPage() {
+  const rooms = useGetRooms();
+
+  if (rooms.isLoading) return <CreateRoomLoading />;
+
+  if (rooms.error || !rooms.data) return <CreateRoomErrorLoading />
+
+  return (
+    <div>
+      <h1>Create room</h1>
+    </div>
+  );
+}
+
+function CreateRoomLoading() {
+  return (
+    <div>
+      loading...
+    </div>
+  );
+}
+
+function CreateRoomErrorLoading() {
+  return (
+    <div>
+      ops, something went wrong!
+    </div>
+  );
 }
--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,26 +1,14 @@
 //* Libraries imports
 import { createFileRoute, Link } from '@tanstack/react-router';
 
-//* Hooks imports
-import { useGetRooms } from "@/hooks/rooms/use-get-rooms";
-
 export const Route = createFileRoute('/')({
   component: App,
 })
 
 function App() {
-  const rooms = useGetRooms();
-
   return (
     <div>
       Hello world
-
-      <pre>
-        {
-          JSON.stringify(rooms.data, null, 2)
-        }
-      </pre>
-
       <div className='flex flex-col gap-2'>
         <Link to="/room">
           Room
