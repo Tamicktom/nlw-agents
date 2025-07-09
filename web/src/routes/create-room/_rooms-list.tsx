87adc292bd0764808802478d459eb149d8bee9ec
Tamicktom <rickpokemon38@gmail.com>
2025-07-09 17:40:50 -0300

    feat: refactor create-room page to use RoomsList component for better separation of concerns

---
+++ web/src/routes/create-room/_rooms-list.tsx
@@ -0,0 +1,40 @@
+//* Hooks imports
+import { useGetRooms } from "@/hooks/rooms/use-get-rooms";
+
+//* Components imports
+import Card from "@/components/ui/card";
+import { RoomCard } from "./_room-card";
+
+export function RoomsList() {
+	const rooms = useGetRooms();
+
+	if (rooms.isLoading) return <CreateRoomLoading />;
+
+	if (rooms.error || !rooms.data) return <CreateRoomErrorLoading />;
+
+	return (
+		<Card.Root>
+			<Card.Header>
+				<Card.Title>Salas recentes</Card.Title>
+				<Card.Description>
+					Acesso rápido para as salas criadas recentemente
+				</Card.Description>
+			</Card.Header>
+
+			<Card.Content className="flex flex-col gap-3">
+				{rooms.data.rooms.map((room) => {
+					const key = room.id;
+					return <RoomCard key={key} room={room} />;
+				})}
+			</Card.Content>
+		</Card.Root>
+	);
+}
+
+function CreateRoomLoading() {
+	return <div>loading...</div>;
+}
+
+function CreateRoomErrorLoading() {
+	return <div>ops, something went wrong!</div>;
+}
--- web/src/routes/create-room/index.tsx
+++ web/src/routes/create-room/index.tsx
@@ -1,53 +1,21 @@
 //* Libraries imports
 import { createFileRoute } from "@tanstack/react-router";
 
-//* Hooks imports
-import { useGetRooms } from "@/hooks/rooms/use-get-rooms";
-
 //* Components imports
-import Card from "@/components/ui/card";
-import { RoomCard } from "./_room-card";
+import { RoomsList } from "./_rooms-list";
 
 export const Route = createFileRoute("/create-room/")({
 	component: CreateRoomPage,
 });
 
 function CreateRoomPage() {
-	const rooms = useGetRooms();
-
-	if (rooms.isLoading) return <CreateRoomLoading />;
-
-	if (rooms.error || !rooms.data) return <CreateRoomErrorLoading />;
-
 	return (
 		<div className="flex flex-col items-center justify-start p-4 min-h-svh bg-zinc-900">
 			<div className="max-w-4xl">
 				<div className="grid items-start grid-cols-2 gap-8">
-					<Card.Root>
-						<Card.Header>
-							<Card.Title>Salas recentes</Card.Title>
-							<Card.Description>
-								Acesso rápido para as salas criadas recentemente
-							</Card.Description>
-						</Card.Header>
-
-						<Card.Content className="flex flex-col gap-3">
-							{rooms.data.rooms.map((room) => {
-								const key = room.id;
-								return <RoomCard key={key} room={room} />;
-							})}
-						</Card.Content>
-					</Card.Root>
+					<RoomsList />
 				</div>
 			</div>
 		</div>
 	);
-}
-
-function CreateRoomLoading() {
-	return <div>loading...</div>;
-}
-
-function CreateRoomErrorLoading() {
-	return <div>ops, something went wrong!</div>;
 }
