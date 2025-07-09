84ed1378c990bd687317a02ed640d9a12e3b28b7
Tamicktom <rickpokemon38@gmail.com>
2025-07-09 19:25:01 -0300

    feat: enhance apiURL function to accept parameters for dynamic URL construction

--- web/src/utils/api.ts
+++ web/src/utils/api.ts
@@ -3,16 +3,37 @@ import { env } from "@/env";
 
 type ValidRoutes = "/rooms";
 
-export function apiURL(validRoute: ValidRoutes) {
-  return parseApiUrl(env.VITE_API_URL) + validRoute;
+type Params = {
+	[key: string]: string | number;
+};
+
+export function apiURL(validRoute: ValidRoutes, params: Params = {}) {
+	let url = parseApiUrl(env.VITE_API_URL) + validRoute;
+
+	const paramsKeys = Object.keys(params);
+
+	let paramsUrl = "?";
+
+	for (const key of paramsKeys) {
+		if (paramsUrl.endsWith("?") || paramsUrl.endsWith("&")) {
+			paramsUrl = `${paramsUrl}${key}=${params[key]}`;
+			continue;
+		}
+
+		paramsUrl = `${paramsUrl}&${key}=${params[key]}`;
+	}
+
+	if (paramsKeys.length > 0) url = url + paramsUrl;
+
+	return url;
 }
 
 function parseApiUrl(apiUrl: string): string {
-  let parsedUrl = apiUrl;
+	let parsedUrl = apiUrl;
 
-  if (parsedUrl.endsWith("/")) {
-    parsedUrl = parsedUrl.slice(0, -1);
-  }
+	if (parsedUrl.endsWith("/")) {
+		parsedUrl = parsedUrl.slice(0, -1);
+	}
 
-  return parsedUrl;
+	return parsedUrl;
 }
