4f9c96a17b486af65081d286b30744dd6d11d895
Tamicktom <rickpokemon38@gmail.com>
2025-07-09 13:32:29 -0300

    feat: implement room listing and storing routes

---
+++ server/src/http/routes/rooms/list.ts
@@ -0,0 +1,56 @@
+//* Libraries imports
+import { count, ilike } from 'drizzle-orm';
+import { Elysia, t } from 'elysia';
+
+//* Local imports
+import { db } from '../../../db/connection';
+import { schema } from '../../../db/schema';
+
+export const listRooms = new Elysia()
+  .get(
+    '/',
+    async (req) => {
+      req.query.page = req.query.page || 1;
+      req.query.limit = req.query.limit || 10;
+      req.query.search = req.query.search || '';
+
+      const roomsToWait = db
+        .select({
+          id: schema.rooms.id,
+          name: schema.rooms.name,
+        })
+        .from(schema.rooms)
+        .where(
+          ilike(schema.rooms.name, `%${req.query.search}%`),
+        )
+        .orderBy(schema.rooms.createdAt)
+        .limit(req.query.limit)
+        .offset((req.query.page - 1) * req.query.limit);
+
+      const totalRoomsToWait = db.select({ value: count() }).from(schema.rooms);
+
+      const [rooms, totalRooms] = await Promise.all([
+        roomsToWait,
+        totalRoomsToWait,
+      ]);
+
+      const pagination = {
+        page: req.query.page,
+        limit: req.query.limit,
+        totalItems: totalRooms[0].value,
+        totalPages: Math.ceil(totalRooms[0].value / req.query.limit),
+      };
+
+      return {
+        rooms,
+        pagination,
+      };
+    },
+    {
+      query: t.Object({
+        page: t.Optional(t.Number({ minimum: 1, default: 1 })),
+        limit: t.Optional(t.Number({ minimum: 1, maximum: 100, default: 10 })),
+        search: t.Optional(t.String()),
+      }),
+    }
+  );
---
+++ server/src/http/routes/rooms/store.ts
@@ -0,0 +1,37 @@
+//* Libraries imports
+import { Elysia, t } from "elysia";
+
+//* Local imports
+import { db } from "../../../db/connection";
+import { rooms } from "../../../db/schema/rooms";
+
+
+const storeRoom = new Elysia();
+
+storeRoom.post("/",
+  async (req) => {
+    const result = await db
+      .insert(rooms)
+      .values({
+        name: req.body.name,
+        description: req.body.description
+      })
+      .returning({
+        id: rooms.id,
+        name: rooms.name,
+        description: rooms.description
+      });
+
+    return ({
+      room: result[0]
+    })
+  },
+  {
+    body: t.Object({
+      name: t.String({ minLength: 3, maxLength: 64 }),
+      description: t.String({ minLength: 0, maxLength: 512 })
+    })
+  }
+);
+
+export { storeRoom };
