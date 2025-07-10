2c899ff2b22e76cfee71982f2adb87e1e22c3306
Tamicktom <rickpokemon38@gmail.com>
2025-07-10 03:37:06 -0300

    feat: refactor QuestionForm component to use Card layout and include roomId prop

--- web/src/routes/room/_question-form.tsx
+++ web/src/routes/room/_question-form.tsx
@@ -3,10 +3,12 @@ import { formOptions, useForm } from "@tanstack/react-form";
 
 //* Componens imports
 import { Button } from "@/components/ui/button";
+import Card from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 
 //* Hooks imports
+import { useStoreRoomQuestion } from "@/hooks/rooms/use-store-room-question";
 
 //* Schemas imports
 import { type StoreQuestion, storeQuestion } from "@/schemas/questions";
@@ -19,55 +21,72 @@ const formOpts = formOptions({
 	defaultValues,
 });
 
-export function QuestionForm() {
+type QuestionFormProps = {
+	roomId: string;
+};
+
+export function QuestionForm(props: QuestionFormProps) {
+	const storeRoomQuestion = useStoreRoomQuestion(props.roomId);
+
 	const form = useForm({
 		...formOpts,
 		validators: {
 			onBlur: storeQuestion,
 		},
 		onSubmit: async (submit) => {
-			console.log(submit);
+			storeRoomQuestion.mutate(submit.value);
 		},
 	});
 
 	return (
-		<form
-			onSubmit={(e) => {
-				e.preventDefault();
-				e.stopPropagation();
-				form.handleSubmit();
-			}}
-		>
-			<form.Field name="question">
-				{(field) => {
-					return (
-						<div className="flex flex-col w-full gap-2">
-							<Label htmlFor="question-text">Sua Pergunta</Label>
-							<Textarea
-								id="question-test"
-								value={field.state.value}
-								onBlur={field.handleBlur}
-								onChange={(e) => field.handleChange(e.target.value)}
-							/>
-							<div className="text-sm text-destructive-foreground">
-								{field.state.meta.errors.map((error, index) => {
-									const key = `error-${index}`;
-									return <span key={key}>{error?.message}</span>;
-								})}
-							</div>
-						</div>
-					);
-				}}
-			</form.Field>
+		<Card.Root>
+			<Card.Header>
+				<Card.Title>Fazer uma pergunta</Card.Title>
+				<Card.Description>
+					Digite a sua pergunta abaixo para receber uma resposta gerado por IA
+				</Card.Description>
+			</Card.Header>
 
-			<Button
-				id="create-question"
-				type="submit"
-				className="w-full"
-				disabled={form.state.isSubmitting}
-			>
-				{form.state.isSubmitting ? "Criando..." : "Criar pergunta"}
-			</Button>
-		</form>
+			<Card.Content>
+				<form
+					onSubmit={(e) => {
+						e.preventDefault();
+						e.stopPropagation();
+						form.handleSubmit();
+					}}
+				>
+					<form.Field name="question">
+						{(field) => {
+							return (
+								<div className="flex flex-col w-full gap-2">
+									<Label htmlFor="question-text">Sua Pergunta</Label>
+									<Textarea
+										id="question-test"
+										value={field.state.value}
+										onBlur={field.handleBlur}
+										onChange={(e) => field.handleChange(e.target.value)}
+									/>
+									<div className="text-sm text-destructive-foreground">
+										{field.state.meta.errors.map((error, index) => {
+											const key = `error-${index}`;
+											return <span key={key}>{error?.message}</span>;
+										})}
+									</div>
+								</div>
+							);
+						}}
+					</form.Field>
+
+					<Button
+						id="create-question"
+						type="submit"
+						className="w-full"
+						disabled={form.state.isSubmitting}
+					>
+						{form.state.isSubmitting ? "Criando..." : "Criar pergunta"}
+					</Button>
+				</form>
+			</Card.Content>
+		</Card.Root>
 	);
 }
