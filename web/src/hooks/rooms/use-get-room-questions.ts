141fec7b341eb594deba72734dade0fe704d3b46
Tamicktom <rickpokemon38@gmail.com>
2025-07-10 03:14:13 -0300

    feat: add useGetRoomsQuestions hook for fetching questions in a room

---
+++ web/src/hooks/rooms/use-get-room-questions.ts
@@ -0,0 +1,49 @@
+//* Libraries imports
+import { useQuery } from "@tanstack/react-query";
+import z from "zod/v4";
+
+import { queryKeys } from "@/integrations/tanstack-query/root-provider";
+//* Local imports
+import { apiURL } from "@/utils/api";
+
+//* Schemas imports
+import { pagination } from "@/schemas/pagination";
+import { paginatedQuestion } from "@/schemas/questions";
+
+const apiResponseSchema = paginatedQuestion.array()
+
+type GetRoomsQuestionsProps = {
+	page?: number;
+	limit?: number;
+	search?: string;
+};
+
+async function getRoomsQuestions(
+	roomId: string,
+	props: GetRoomsQuestionsProps,
+) {
+	const params = {
+		page: props.page || 1,
+		limit: props.limit || 10,
+		search: props.search || "",
+	};
+
+	const url = apiURL(`/rooms/${roomId}/questions`, params);
+
+	const response = await fetch(url);
+
+	const data = apiResponseSchema.parse(await response.json());
+
+	return data;
+}
+
+export function useGetRoomsQuestions(
+	roomId: string,
+	props: GetRoomsQuestionsProps = {},
+) {
+	return useQuery({
+		queryKey: [queryKeys.rooms.list],
+		queryFn: async () => await getRoomsQuestions(roomId, props),
+		staleTime: 1000 * 60 * 5, // 5 minutes
+	});
+}
